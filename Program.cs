b8d6ba578f78c32871c19dcf6561c77ac10a9916
Nikita Sychev <nikitasychev@inbox.ru>
2022-12-11 12:21:28 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_Word
+{
+    class FileIO
+    {
+        static private int x;
+    }
+    class MyFile : FileIO
+    {
+        public MyFile()
+        {
+            Console.WriteLine(FileIO.x);
+        }
+
+    }
+    internal class Program
+    {
+        static void PrintName(string _name, string _order = "fgsdrg")
+        {
+            Console.WriteLine("Hello, ");
+            Console.Write("Hello, \n");
+            if (_order == "-")
+            {
+                for (int i = _name.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(_name[i] + "\n");
+                }
+            }
+            else
+            {
+
+                for (int i = 1; i < _name.Length; i+=2)
+                {
+                    Console.Write(_name[i] + "\n");
+                }
+
+            }
+            Console.WriteLine("!");
+        }
+        static void Main(string[] args)
+        {
+            string name = "", order = "";
+            Console.WriteLine(value: FileIO.x);
+            try
+            {
+                name = args[0];
+                order = args[1];
+            }
+            catch
+            {
+                Console.WriteLine("Как вас зовут?");
+                name = Console.ReadLine();
+            }
+            finally
+            {
+                PrintName(name, order);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
